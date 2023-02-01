d787c88d67fa96f673e676a9046b2f875033459f
pockos56 <103532988+pockos56@users.noreply.github.com>
2023-02-01 17:43:41 +0100

    Functionised and implemented residuals. also new plots

--- FP optimization.jl
+++ FP optimization.jl
@@ -145,7 +145,7 @@ CSV.write("C:\\Users\\alex_\\Documents\\GitHub\\IE_prediction\\Morgan_FP_optimis
 CSV.write("C:\\Users\\alex_\\Documents\\GitHub\\IE_prediction\\Morgan_FP_optimisation_results_plus.csv", z_df_sorted_plus)
 
 ## Importance for Padel-12 ##
-function par_importance(ESI)
+function parameter(ESI; allowplots=false, allowsave=false)
     if ESI == -1
         ESI_name = "minus"
         output = data_minus[:,3]
@@ -154,62 +154,73 @@ function par_importance(ESI)
         output = data_plus[:,3]
     else error("Set ESI to -1 or +1 for ESI- and ESI+ accordingly")
     end
-    FP1 = Matrix(CSV.read("C:\\Users\\alex_\\Documents\\GitHub\\IE_prediction\\Fingerprints\\padel_$(ESI_name)_12.csv", DataFrame)[:,2:end])
-    X_train, X_test, y_train, y_test = train_test_split(FP1, output, test_size=0.20, random_state=2);
+    FP1 = CSV.read("C:\\Users\\alex_\\Documents\\GitHub\\IE_prediction\\Fingerprints\\padel_$(ESI_name)_12.csv", DataFrame)[:,2:end]
+    X_train, X_test, y_train, y_test = train_test_split(Matrix(FP1), output, test_size=0.20, random_state=2);
     reg = RandomForestRegressor(n_estimators=600, min_samples_leaf=4, max_features=(Int64(ceil(size(FP1,2)/3))), n_jobs=-1, oob_score =true, random_state=2)
     fit!(reg, X_train, y_train)
     importance = 100 .* sort(reg.feature_importances_, rev=true)
     importance_index = sortperm(reg.feature_importances_, rev=true)
     significant_columns = importance_index[importance .>=1]
-    important_desc = names(FP1[:,:])[significant_columns]   
+    z1 = names(FP1[:,:])[significant_columns]   # Most important descriptors
+    z2 = score(reg, X_train, y_train)   # Train set accuracy
+    z3 = score(reg, X_test, y_test)      # Test set accuracy
+    z4 = predict(reg,X_train)     # y_hat_train
+    z5 = predict(reg,X_test)   # y_hat_test
+    z6 = z4 - y_train      # Train set residual
+    z7 = z5 - y_test        # Test set residual
+    
+    if allowplots == true
+        plot1 = scatter(y_train,z4,label="Training set", legend=:best, title = "ESI $(ESI_name)- IEs from FP", color = :magenta, xlabel = "Experimental log(IE)", ylabel = "Predicted log(IE)")
+        scatter!(y_test,z5,label="Test set", color=:orange)
+        plot!([minimum(vcat(y_train,y_test)),maximum(vcat(y_train,y_test))],[minimum(vcat(y_train,y_test)),maximum(vcat(y_train,y_test))],label="1:1 line",width=2)
+        if allowsave ==true
+            savefig("C:\\Users\\alex_\\Documents\\GitHub\\IE_prediction\\RF_Regression_$ESI_name.png")
+        end
+
+        plot2 = scatter(y_train,z6,label="Training set", legend=:best, title = "ESI $(ESI_name)- Regression residuals", color = :magenta, xlabel = "Experimental log(IE)", ylabel = "Residual")
+        scatter!(y_test,z7, label="Test set",color=:orange)
+        plot!([minimum(vcat(y_test,y_train)),maximum(vcat(y_test,y_train))],[0,0],label="1:1 line",width=2) # 1:1 line
+        plot!([minimum(vcat(y_test,y_train)),maximum(vcat(y_test,y_train))],[3*std(vcat(z6,z7)),3*std(vcat(z6,z7))],label="+/- 3 std",linecolor ="grey",width=2) # +3 sigma
+        plot!([minimum(vcat(y_test,y_train)),maximum(vcat(y_test,y_train))],[-3*std(vcat(z6,z7)),-3*std(vcat(z6,z7))],label=false,linecolor ="grey",width=2) #-3 sigma
+
+        if allowsave ==true
+            savefig("C:\\Users\\alex_\\Documents\\GitHub\\IE_prediction\\RF_Residuals_$ESI_name.png")
+        end
+        display(plot1)
+        display(plot2)
+    end
+return z1,z2,z3,z4,z5,z6,z7
 end
-par_importance(+1)
-ESI_name = "plus"
-FP1 = CSV.read("C:\\Users\\alex_\\Documents\\GitHub\\IE_prediction\\Fingerprints\\padel_$(ESI_name)_12.csv", DataFrame)[:,2:end]
-X_train, X_test, y_train, y_test = train_test_split(Matrix(FP1), output, test_size=0.20, random_state=2);
-reg = RandomForestRegressor(n_estimators=600, min_samples_leaf=4, max_features=(Int64(ceil(size(FP1,2)/3))), n_jobs=-1, oob_score =true, random_state=2)
-fit!(reg, X_train, y_train)
 
-## Calculation of R2
-accuracy_train = score(reg, X_train, y_train)
-accuracy_test = score(reg, X_test, y_test)
 
-## Prediction of IEs
-y_hat_test = predict(reg,X_test)
-y_hat_train = predict(reg,X_train)
+importance_minus, accuracy_tr_minus, accuracy_te_minus, y_hat_train_minus, y_hat_test_minus, res_train_minus, res_test_minus = parameter(-1, allowplots=true, allowsave=true)
+importance_plus, accuracy_tr_plus, accuracy_te_plus, y_hat_train_plus, y_hat_test_plus, res_train_plus, res_test_plus = parameter(+1, allowplots=true, allowsave=true)
 
-scatter(y_train,y_hat_train,label="Training set", legend=:topleft, title = "IE from FP-all descriptors", color = :magenta, xlabel = "Experimental IE", ylabel = "Predicted IE")
-scatter!(y_test,y_hat_test,label="Test set",legend=:topleft, color=:orange)
-plot!([-0.5,2.5],[-0.5,2.5],label="1:1 line",linecolor ="black",width=2)
-savefig("C:\\Users\\alex_\\Documents\\GitHub\\IE_prediction\\Regression_$ESI_name.png")
 
-## Residuals
-res_train = y_hat_train - y_train
-res_test = y_hat_test - y_test
+importance_minus
+importance_plus
+
 
 ## Cross Validation
 CV = cross_val_score(reg, X_train, y_train, cv=5)
 CV_mean = mean(CV)
 
-## Importance ##
-importance = 100 .* sort(reg.feature_importances_, rev=true)
-importance_index = sortperm(reg.feature_importances_, rev=true)
-significant_columns = importance_index[importance .>=1]
-important_desc = names(FP1[:,:])[significant_columns]   
-
-## Meeting notes ##
-#  Morgan Fingerprints (if time) --> DONE
-## Importance
-## Inter-Correlation
-## Positive mode
+####### Meeting notes #######
+## DONE ##
+# Morgan Fingerprints (if time)
+# Positive mode
 # Residuals + plots
-## Find abnormalities to show model strengths and weaknesses
-## Different types of models (CATBOOST, XgBoost)
+
+## IN PROGRESS ##
+# Importance --> WRITE IN TABLE
+
+## TO DO ##
+# Inter-Correlation
+# Find abnormalities to show model strengths and weaknesses
+# Different types of models (CATBOOST, XgBoost)
 # CATBOOST -> Learning rate, LU1_reg, LU2_reg
 # XgBoost -> Learning rate & RandomForestRegressor
 
 ## CNL ##
-## InChI keys creation
-## Talk with Denice
-
-
+# InChI keys creation
+# Talk with Denice
