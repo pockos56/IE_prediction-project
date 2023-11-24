32b062c9b39d01ebfcaecb02fb4495a777d04d78
pockos56 <103532988+pockos56@users.noreply.github.com>
2023-11-24 14:47:46 +0100

    Creation of expanded data

--- Unified/code/Expansion.jl
+++ Unified/code/Expansion.jl
@@ -1,4 +1,4 @@
-using CSV, DataFrames, PyCall, Conda, ProgressBars, Statistics, LinearAlgebra, Plots, Random
+using CSV, DataFrames, PyCall, Conda, ProgressBars, Statistics, LinearAlgebra, Plots, Random, DataStructures
 #using Pkg
 #Pkg.add(url="https://github.com/pockos56/IE_prediction.jl")
 using IE_prediction
@@ -123,7 +123,7 @@ end
 not_calculated = findall(x->x.==-Inf, expa_spectra_wpH[:,"logIE"])
 CSV.write("C:\\Users\\alex_\\Documents\\GitHub\\IE_prediction-project\\Unified\\data\\CNL-IE\\Expansion.csv", expa_spectra_wpH)
 
-# Load data
+# Load data for combination of original + expansion
 CNL_IE_unified_zero = CSV.read("C:\\Users\\alex_\\Documents\\GitHub\\IE_prediction-project\\Unified\\data\\CNL-IE\\CNL_IE_unified_zero.csv", DataFrame)
 expansion = CSV.read("C:\\Users\\alex_\\Documents\\GitHub\\IE_prediction-project\\Unified\\data\\CNL-IE\\Expansion.csv", DataFrame)
 
@@ -135,7 +135,99 @@ unique(expansion[:,"INCHIKEY"])             # 835 unique INCHIKEYs
 findall(x -> x in unique(CNL_IE_unified_zero[:,"INCHIKEY"]), unique(expansion[:,"INCHIKEY"]))   # No inchikey overlap
 
 # Prepare expansion set to CNL binary format
+best_CNLs_pos = CSV.read("C:\\Users\\alex_\\Documents\\GitHub\\IE_prediction-project\\data\\CNLmax_Hadducts_pos.CSV", DataFrame)[1:500,1]
+function create_expansion_dataset(;ESI=+1, threshold=0.02, HiValSetToZero::Bool=true)
+    function NIST_df(input)
+        function mz_to_fingerprint(str::String, precursor::Float64, ESI::Int; threshold)
+            # Convert string to vector{float64}
+            str = replace(str, r"Any(\[.*?\])" => s"\1")
+            mz_vec = eval(Meta.parse(str))
+            CNL_vec = round.((precursor .- mz_vec)[(precursor .- mz_vec).>=0],digits = 4)
+            # Define the range and step size for the fingerprint
+            fingerprint_range_raw = []
+            if ESI == -1
+                fingerprint_range_raw = round.(best_CNLs_neg, digits=2)
+            elseif ESI == +1
+                fingerprint_range_raw = round.(best_CNLs_pos, digits=2)
+            else
+                error("Put ESI to +1/-1")
+            end
+            distance_threshold = threshold - 0.01
+            fingerprint_range = fingerprint_range_raw[1:2]
+            distance=[]
+            #fingerprint_range = vcat(fingerprint_range, 18.0)
+            for i = 3:length(fingerprint_range_raw)
+                distance = round.(abs.(fingerprint_range_raw[i] .- fingerprint_range),digits=3)
+                if all(distance .> distance_threshold) #isempty(distance[distance .<= distance_threshold])
+                    push!(fingerprint_range, fingerprint_range_raw[i])
+                end
+            end
+                
+            # Initialize the fingerprint dictionary
+            fingerprint_dict = sort(Dict([(x, 0) for x in fingerprint_range]))
+            # Loop over the values and update the fingerprint dictionary
+            for v in CNL_vec
+                # Find the nearest index in the fingerprint_range (FIX TO SET TO ONE WITH A BINNING OF 0.01)
+                if findmin(abs.(fingerprint_range .- v))[1] <= threshold
+                    idx = findmin(abs.(fingerprint_range .- v))[2]
+                    # Increment the count for that index in the fingerprint dictionary
+                    fingerprint_dict[fingerprint_range[idx]] = 1
+                end
+            end
+            # Convert the fingerprint dictionary to a DataFrame
+            dict_str = OrderedDict(string(k) => v for (k, v) in fingerprint_dict)
+            fingerprint_df = DataFrame(dict_str)
+            # Change all values higher than the precursor ion to -1
+            idx_precursor = findmin(abs.(Meta.parse.(names(fingerprint_df)) .- precursor))[2]
+            if HiValSetToZero == false
+                fingerprint_df[:,idx_precursor+1:end] .= -1         # Set to -1, but could be changed
+            elseif HiValSetToZero == true
+                fingerprint_df[:,idx_precursor+1:end] .= 0         # Set to zero, but could be changed
+            end
+            return fingerprint_df
+        end
+        
+        df = DataFrame()
+        features_w_order = [:ACCESSION, :NAME,:COLLISION_ENERGY, :INCHIKEY, :SMILES, :FORMULA, :EXACT_MASS]
+        for i = 1:size(input,1)
+            df_temp_CNL = mz_to_fingerprint(input[i,:MZ_VALUES], input[i,:PRECURSOR_ION], ESI; threshold)
+
+            df_temp_info = DataFrame(Matrix(DataFrame(input[i,features_w_order])),["Column1","NAME","RI_pred","INCHIKEY","SMILES","FORMULA","MONOISOMASS"])
+            df_temp = hcat(df_temp_info, df_temp_CNL)
+            append!(df, df_temp)
+            # histogram(Vector(df[1,8:end]))
+            if (i % 250)==0
+            println("NIST $ESI Convert to DF-Compound $i out of $(size(input,1))")
+            end
+        end
+        return df
+    end
+    NIST = CSV.read("C:\\Users\\alex_\\Documents\\GitHub\\IE_prediction-project\\Unified\\data\\CNL-IE\\Expansion.csv", DataFrame)
+    NIST_ESI = ()
+    unique(Vector(expansion[:,:ION_MODE]))
+    if ESI == -1
+        NIST_ESI_ionmode = vcat(NIST[Vector(NIST[:,:ION_MODE]).=="N",:],NIST[Vector(NIST[:,:ION_MODE]).=="NEGATIVE",:])
+        NIST_ESI = NIST_ESI_ionmode[((NIST_ESI_ionmode[:,:EXACT_MASS] - NIST_ESI_ionmode[:,:PRECURSOR_ION]) .< 1.01),:]
+    elseif ESI == +1
+        NIST_ESI_ionmode = vcat(NIST[Vector(NIST[:,:ION_MODE]).=="P",:],NIST[Vector(NIST[:,:ION_MODE]).=="POSITIVE",:])
+        NIST_ESI = NIST_ESI_ionmode[((NIST_ESI_ionmode[:,:PRECURSOR_ION] - NIST_ESI_ionmode[:,:EXACT_MASS]) .< 1.01),:]
+    else error("Set ESI to -1 or +1 for ESI- and ESI+ accordingly")
+    end
+    
+    dataset = NIST_df(NIST_ESI[:,:])
+    dataset[!,:FORMULA] = convert.(String, dataset[!,:FORMULA])
+    select!(dataset, Not([:RI_pred]))
+    insertcols!(dataset, 4, :pH_aq => NIST_ESI[:,"pH_aq"])  # That's the pH column
+    insertcols!(dataset, 4, :logIE => NIST_ESI[:,"logIE"])  # That's the IE column
+
+    return dataset
+end
+CNL_IE_expansion = create_expansion_dataset()
+CSV.write("C:\\Users\\alex_\\Documents\\GitHub\\IE_prediction-project\\Unified\\data\\CNL-IE\\CNL-IE_Expansion.csv", CNL_IE_expansion)
 
 # Combine data
-names(CNL_IE_unified_zero)
-names(expansion)
+names(CNL_IE_unified_zero) == names(CNL_IE_expansion)   # Same colnames => Ready for vcat
+CNL_IE_unified_zero = CSV.read("C:\\Users\\alex_\\Documents\\GitHub\\IE_prediction-project\\Unified\\data\\CNL-IE\\CNL_IE_unified_zero.csv", DataFrame)
+CNL_IE_expansion = CSV.read("C:\\Users\\alex_\\Documents\\GitHub\\IE_prediction-project\\Unified\\data\\CNL-IE\\CNL-IE_Expansion.csv", DataFrame)
+CNL_IE_after_expansion = vcat(CNL_IE_unified_zero, CNL_IE_expansion)
+CSV.write("C:\\Users\\alex_\\Documents\\GitHub\\IE_prediction-project\\Unified\\data\\CNL-IE\\CNL_IE_after_expansion.csv", CNL_IE_after_expansion)
